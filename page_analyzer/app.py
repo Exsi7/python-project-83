fca7fa9561bb25fd147d7497e76b54615530f257
Viktor Zotov <ZotovV777@yandex.ru>
2023-04-05 23:43:59 +0300

    test

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -14,6 +14,8 @@ from datetime import date
 
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
@@ -25,11 +27,6 @@ def project_3():
 
 @app.post('/urls')
 def url_post():
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-    except Exception:
-        print('Can`t establish connection to database')
     data_dict = request.form.to_dict()
     data = data_dict['url']
     if validators.url(data, public=True) and len(data) <= 255:
@@ -52,11 +49,6 @@ def url_post():
 
 @app.route('/urls/<id>')
 def page_url(id):
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-    except Exception:
-        print('Can`t establish connection to database')
     with conn.cursor() as curs:
         curs.execute('SELECT * FROM urls WHERE id=%s', (id,))
         url = curs.fetchone()

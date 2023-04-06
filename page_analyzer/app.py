20c86e2e95199681801375316eb19de47c66586c
Viktor Zotov <ZotovV777@yandex.ru>
2023-04-06 23:35:01 +0300

    test

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -16,6 +16,8 @@ from datetime import date
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 print(DATABASE_URL)
+print(DATABASE_URL)
+print(DATABASE_URL)
 conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)

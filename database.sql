6a2945cf622f5fc24a118e42d384ae4a6300f7d9
Exsi7 <ZotovV777@yandex.ru>
2023-04-17 19:35:39 +0300

    test

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id serial PRIMARY KEY,
     name varchar(255),
     created_at timestamp
 );
